2601bc50fb0d69ee406a51d9f644bedb0442338e
NareshP10 <naresh.raj@people10.com>
2023-10-23 16:48:55 +0530

    first commit

---
+++ DatingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public UsersController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = this.context.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id) 
+        {
+            var user = this.context.Users.Find(id);
+            return user;
+        }
+    }
+}
